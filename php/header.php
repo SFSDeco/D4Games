4f31c86bf12477b4ba681f5c56ed10e0ae272589
David Ung <david.kim.ung@gmail.com>
2023-04-12 16:02:32 +0000

    Updated header.php with correct href to css

--- php/header.php
+++ php/header.php
@@ -4,5 +4,5 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     
-    <link rel="stylesheet" href="../D4styles.css">
+    <link rel="stylesheet" href="D4styles.css">
 </head>
